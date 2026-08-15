62d20de2bf5850157fedcb36a9fdde27fa7ff06b
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-08-15 19:52:22 +0530

    Python Fundamentals - 3: Strings.

---
+++ Python Fundamentals 3/__init__.py
@@ -0,0 +1,19 @@
+# Strings:
+
+word = "Python"
+word2 = "Programming"
+
+print(word)
+print(word2)
+
+print(len(word))
+print(len(word2))
+
+conStr = word + " " + word2
+
+print(conStr)
+
+print(word[2])
+
+for letter in word:
+    print(letter)
