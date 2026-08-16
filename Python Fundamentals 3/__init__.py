ea5192c9df9b619d33f454b12d19faa954be095b
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-08-16 20:49:39 +0530

    Python Fundamentals - 3: Strings - String Slicing.

--- Python Fundamentals 3/__init__.py
+++ Python Fundamentals 3/__init__.py
@@ -1,4 +1,4 @@
-# Strings:
+# Strings in Python:
 
 word = "Python"
 word2 = "Programming"
@@ -17,3 +17,16 @@ print(word[2])
 
 for letter in word:
     print(letter)
+
+# String Slicing:
+str1 = "Python"
+print(str1[2:4])
+
+str2 = "I study from Apna College"
+print(str2[13:25])
+print(str2[13:])
+print(str2[13:len(str2)])
+print(str2[:])
+
+str3 = "Python"
+print(str3[-4:-2])
