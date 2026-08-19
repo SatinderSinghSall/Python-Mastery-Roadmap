846eab8a572c947a0e59a0c89f62926b917e32b8
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-08-19 17:14:54 +0530

    Python Fundamentals - 4: Object-Oriented Programming: # Attributes & Methods: OOP in Python.

--- Python Fundamentals 4/__init__.py
+++ Python Fundamentals 4/__init__.py
@@ -6,24 +6,50 @@
 
 # Class:
 class Student:
+    def __init__ (self, name, subject, university, year_of_admission):
+        self.name = name
+        self.subject = subject
+        self.university = university
+        self.year_of_admission = year_of_admission
+        print("Constructor called")
+
     subject = "Python Programming"
     university = "University"
     year_of_admission = "2020"
 
 # Object:
-student = Student()
+student = Student("Satinder Singh Sall", "Java Programming", "REVA University", "2020")
 print(student)
+print(student.name)
+print(student)
+print(student.name)
+print(student.subject)
+print(student.university)
+print(student.year_of_admission)
+print()
 print(student.subject)
 print(student.university)
 print(student.year_of_admission)
 
-student2 = Student()
+print()
+print()
+
+student2 = Student("Soni Vaibhav Kumar", "C++ Programming", "REVA University", "2020")
 print(student2)
+print(student2.name)
 print(student2.subject)
 print(student2.university)
 print(student2.year_of_admission)
+print()
+print(student2.subject)
+print(student2.university)
+print(student2.year_of_admission)
+
+print()
+print()
 
 print(student.subject, student.university, student.year_of_admission)
 print(student2.subject, student2.university, student2.year_of_admission)
 
 # Attributes & Methods: OOP
+# Constructor: init() method (to initialize the objects)
