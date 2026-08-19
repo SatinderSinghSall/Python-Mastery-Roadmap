3de6e3d0b65f81ef90c98f3c7d56ae486e6a4939
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-08-19 14:05:36 +0530

    Python Fundamentals - 3: Lists in Python.

---
+++ Python Fundamentals 3/Lists.py
@@ -0,0 +1,30 @@
+# Lists in Python: Mutable sequence of values.
+
+marks = [99, 89, 100, 65, 92]
+
+print(marks)
+print(type(marks))
+print(len(marks))
+
+print(marks[1])
+
+marks[1] = 100
+print(marks)
+
+print(marks[1:3])
+
+# List Methods: Functions
+nums = [1, 2, 3, 4, 5]
+print(nums)
+
+nums.append(6)
+print(nums)
+
+nums.insert(1, 7)
+print(nums)
+
+nums.remove(1)
+print(nums)
+
+nums.sort()
+print(nums)
