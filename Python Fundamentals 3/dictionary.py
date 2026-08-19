9df7dad4eaa620744dbe9020ca6132e2f980f47c
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-08-19 14:26:41 +0530

    Python Fundamentals - 3: Dictionary in Python.

--- Python Fundamentals 3/dictionary.py
+++ Python Fundamentals 3/dictionary.py
@@ -1,3 +1,22 @@
 # Dictionary in Python: Key & Value Pair
 
+info = {
+    "name": "Satinder Singh Sall",
+    "age": 22,
+    "subjects": ["Math", "English"],
+    "CGPA": 6.9
+}
 
+print(info)
+print(type(info))
+print(info["name"])
+
+info["CGPA"] = 10
+print(info)
+
+# Dictionary Method:
+print(info.keys())
+print(info.values())
+print(info.items())
+print(info.get("CGPA"))
+print(info.update({"CGPA": 5}))
