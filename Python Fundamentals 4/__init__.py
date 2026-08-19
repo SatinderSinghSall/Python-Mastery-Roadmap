8b88e908e0df15ca9e4b17a030f59857776d6c28
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-08-19 16:57:21 +0530

    Python Fundamentals - 4: Object-Oriented Programming: Objects & Classes in Python.

--- Python Fundamentals 4/__init__.py
+++ Python Fundamentals 4/__init__.py
@@ -3,3 +3,25 @@
 # Classes and Object:
 # Class is a blueprint of an Object.
 # An Object is an instance of a class.
+
+# Class:
+class Student:
+    subject = "Python Programming"
+    university = "University"
+    year_of_admission = "2020"
+
+# Object:
+student = Student()
+print(student)
+print(student.subject)
+print(student.university)
+print(student.year_of_admission)
+
+student2 = Student()
+print(student2)
+print(student2.subject)
+print(student2.university)
+print(student2.year_of_admission)
+
+print(student.subject, student.university, student.year_of_admission)
+print(student2.subject, student2.university, student2.year_of_admission)
