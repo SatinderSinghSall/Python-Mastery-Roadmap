ebc29a5baef22c8e3f50f927d65326dc4045956e
Satinder Singh Sall <satindersinghsall111@gmail.com>
2026-08-22 12:23:53 +0530

    Python Fundamentals - 4: Object-Oriented Programming: Methods: Instance, Class & Static in Python.

--- Python Fundamentals 4/methods.py
+++ Python Fundamentals 4/methods.py
@@ -1,3 +1,24 @@
 # Methods: Instance, Class & Static
 
+class Laptop:
+    storage_type = "SSD"
 
+    def __init__(self, ram, storage):
+        self.ram = ram
+        self.storage = storage
+
+    @classmethod
+    def get_storage_type(cls):
+        print(f"Storage Type: {cls.storage_type}")
+
+    def get_info(self):
+        print(f"Laptop has {self.ram} RAM and {self.storage} {self.storage_type} storage.")
+
+laptop1 = Laptop("16GB", "512GB")
+laptop2 = Laptop("8GB", "256GB")
+
+laptop1.get_info()
+laptop2.get_info()
+
+print(laptop1.get_storage_type())
+print(laptop2.get_storage_type())
